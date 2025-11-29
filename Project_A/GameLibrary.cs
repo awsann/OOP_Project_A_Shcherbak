79c8321ba55d61bda74b8d7d01b8ad32ba961bc4
Anna <a.v.shcherbak@student.khai.edu>
2025-11-29 16:50:19 +0200

    Додано клас GameLibrary із заглушками

---
+++ Project_A/GameLibrary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    public class GameLibrary
+    {
+        public string Name { get; set; }
+        public List<VideoGame> Games { get; set; } // композиція
+        public List<GameSession> Sessions { get; set; } // композиція
+
+        public GameLibrary(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGame(VideoGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveGame(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameSession StartSession(VideoGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VideoGame> GetAllGames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
