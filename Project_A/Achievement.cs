b156811e1d9e6d377384a86b919cb63721a511cb
Anna <a.v.shcherbak@student.khai.edu>
2025-11-29 16:48:28 +0200

    Додано клас Achievement із заглушками

---
+++ Project_A/Achievement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    public class Achievement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Points { get; set; }
+
+        public Achievement(string title, string description, int points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unlock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
