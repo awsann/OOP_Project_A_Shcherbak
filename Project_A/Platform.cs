54d474c7ecfc791c47f6d1f276ffc161222a006d
Anna <a.v.shcherbak@student.khai.edu>
2025-11-29 16:47:57 +0200

    Додано клас Platform із заглушками

---
+++ Project_A/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    public class Platform
+    {
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Platform(string name, int releaseYear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
