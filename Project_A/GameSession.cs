f824c28f5b75ffd510604149b05b8893ec4e03ca
Anna <a.v.shcherbak@student.khai.edu>
2025-11-29 16:49:47 +0200

    Додано клас GameSession із заглушками

---
+++ Project_A/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    public class GameSession
+    {
+        public int SessionId { get; set; }
+        public VideoGame Game { get; set; } // асоціація
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+
+        public GameSession(int sessionId, VideoGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
